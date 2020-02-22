4bff5ecee25f21b43d6187d3116fa503f31374d8
Maminspapin <y.lyulchenko@mail.ru>
2020-02-22 20:17:12 +0700

    Renamed the package "Main" to "main"

---
+++ src/main/java/main/StreamsStarter.java
@@ -0,0 +1,19 @@
+package main;
+
+import streams.EventStream;
+import streams.ResultStream;
+import org.apache.kafka.streams.KafkaStreams;
+
+public class StreamsStarter {
+
+    public static void main(String[] args) {
+
+        // TODO добавить логирование
+        // TODO добавить обработку исключений
+        KafkaStreams eventStream = EventStream.newStream();
+        KafkaStreams resultStream = ResultStream.newStream();
+
+        eventStream.start();
+        resultStream.start();
+    }
+}
